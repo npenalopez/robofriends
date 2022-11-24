829a313a1d5d18cf920d1b914a39ddfa10bb35b1
Nestor Peña López <nestor.penalopez@comfone.com>
2022-11-24 11:00:55 +0100

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
